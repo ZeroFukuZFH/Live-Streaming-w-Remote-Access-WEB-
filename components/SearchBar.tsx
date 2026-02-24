66cd22969798f45f72a3350d9c2993bd8e8fc062
ZeroFuku <12mnsin@gmail.com>
2026-02-24 15:35:45 +0800

    typescript compliance

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { Search } from "lucide-react";
 import { useSearch } from "./DataFetch";
 
@@ -12,14 +12,18 @@ export function SearchBar() {
     console.log(result)
   },[result,search])
 
-  const handleSearch = (e) => {
+  const handleSearch = (e:ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   return <>
     <div className="border border-gray-700 rounded-lg p-2 flex flex-row bg-gray-900 w-full">
       <Search className="text-gray-400 mr-2" size={20} />
-      <input className="outline-none bg-transparent w-full text-white placeholder-gray-500" placeholder="Titles, Peoples, Genre" value={search} onChange={handleSearch} />
+      <input className="outline-none bg-transparent w-full text-white placeholder-gray-500" 
+        placeholder="Titles, Peoples, Genre" 
+        value={search} 
+        onChange={handleSearch} 
+      />
     </div>
     {search.length !== 0 && 
     <div className="flex flex-col">
